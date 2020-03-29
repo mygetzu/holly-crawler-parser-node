167a23ea35b98ea71f3b3c301c01319c665ba413
mygetzu <mbenypangestu@gmail.com>
2020-03-29 21:08:44 +0700

    Update bin

--- bin/run.development.sh
+++ bin/run.development.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+pm2 stop holly-crawler
 git pull origin master
-ps aux | grep 'node /home/mygetzu/projects/holly-crawler-parser-node/node_modules/.bin/ts-node' | grep -v grep | awk '{print $2}' | xargs kill
-nohup npm run start &
+pm2 start holly-crawler
